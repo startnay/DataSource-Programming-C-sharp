2a5833a6c583038a659dd8efb4e7e5d0dd293a17
startnay <start.nay@gmail.com>
2017-03-18 12:45:27 +0700

    start-project

---
+++ DataSource-Programming-C-sharp/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSource_Programming_C_sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string connectionString = "Data Source=(local);" +
+                                        "Initial Catalog=MMABooks;" + 
+                                        "Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+        }
+    }
+}
